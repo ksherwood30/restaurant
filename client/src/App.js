42e0c8c5c41d2820f6bfd02af7794da864b74d9c
Kyle Sherwood <kylesherwood20@gmail.com>
2021-03-31 13:26:00 -0400

    add exact keyword to router

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,21 @@ import  Centermenu from './components/menu/centermenu';
 
 import  {Footer} from './components/layout/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     // <Router>
       <div className="App">
         <Navbar/>
-        <Route exact path="/" component={Landing}/>
-        <Route exact path="/pizza"  render={(props) => <Centermenu page="pizza" {...props} />}/>
-        <Route exact path="/burgers" render={(props) => <Centermenu page="burgers" {...props} />}/>
-        <Route exact path="/wings" render={(props) => <Centermenu page="wings" {...props} />}/>
-        <Route exact path="/pasta" render={(props) => <Centermenu page="pasta" {...props} />}/>
-        <Route exact path="/checkout" component={Checkout}/>
+        <Switch>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/pizza"  render={(props) => <Centermenu page="pizza" {...props} />}/>
+          <Route exact path="/burgers" render={(props) => <Centermenu page="burgers" {...props} />}/>
+          <Route exact path="/wings" render={(props) => <Centermenu page="wings" {...props} />}/>
+          <Route exact path="/pasta" render={(props) => <Centermenu page="pasta" {...props} />}/>
+          <Route exact path="/checkout" component={Checkout}/>
+        </Switch>
         {/* <Route path="/tacos" render={(props) => <Centermenu page="tacos" {...props} />}/>
         <Route path="/sides" render={(props) => <Centermenu page="sides" {...props} />}/> */}
         <Footer/>      
